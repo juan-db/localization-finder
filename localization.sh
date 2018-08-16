93754333cf22a6aad4805ab45e0d4eb807eac600
Juan de Bruin <27200735+juan-db@users.noreply.github.com>
2018-08-16 12:14:38 +0200

    Add colored output

--- localization.sh
+++ localization.sh
@@ -14,14 +14,22 @@ MATCHES2=`egrep -o -e "l\('[^']*'\)" $FILES`
 MATCHES=$MATCHES1+$MATCHES2
 
 REGEX="(.*)\:l\('([^']*)'\)$"
+FOUND=0
 for match in ${MATCHES[@]}; do
 	if [[ $match =~ $REGEX ]]; then
 		printf "Checking for ${BASH_REMATCH[2]}"
 		if ! grep -q -e "name=\"${BASH_REMATCH[2]}\"" $LOCALIZATION_FILE; then
 			printf "\033[32767D\033[2K"
-			printf "Name ${BASH_REMATCH[2]} in file ${BASH_REMATCH[1]}\n"
+			printf "Name \033[1;31m${BASH_REMATCH[2]}\033[0m in file ${BASH_REMATCH[1]}\n"
+			((FOUND++))
 		else
 			printf "\033[2K\033[32767D"
 		fi
 	fi
 done
+
+if [[ $FOUND -ne 0 ]]; then
+	printf "\033[31m%d unlocalized names found" $FOUND
+else
+	printf "\033[32mNo unlocalized names found"
+fi
